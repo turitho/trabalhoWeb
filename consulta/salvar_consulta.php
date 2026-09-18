b0c58addfbad712a51b00d52faf5210f90d20374
Luiz F do Santos <luiz.santos26@estudante.ifms.edu.br>
2026-09-18 07:59:26 -0400

    Refactor salvar_consulta.php to streamline data handling

--- consulta/salvar_consulta.php
+++ consulta/salvar_consulta.php
@@ -1,21 +1,19 @@
 <?php
+  $dados = array();
+  $dados['id'] = $_POST['id'];
+  $dados['data_hora'] = $_POST['data_hora'];
+  $dados['diagnostico'] = $_POST['diagnostico'];
+  $dados['id_medico'] = $_POST['id_medico'];
+  $dados['id_paciente'] = $_POST['id_paciente'];
+  $dados['gravidade'] = $_POST['gravidade'];
 
-    $dados = array();
+  include "funcao.php";
 
-    $dados['data_hora'] = $_POST['data_hora'];
-
-    $dados['diagnostico'] = $_POST['diagnostico'];
-
-    $dados['id_medico'] = $_POST['id_medico'];
-
-    $dados['id_paciente'] = $_POST['id_paciente'];
-
-    $dados['gravidade'] = $_POST['gravidade'];
-
-    include "funcao.php";
-
+  if ($dados['id'] == 0 || $dados['id'] == '') {
     insertConsulta($dados);
+  } else {
+    updateConsulta($dados);
+  }
 
-    header('location: consultas.php');
-
+  header('location: consultas.php');
 ?>
