4ade3e5b6329805c8f32b416d437b9fc3809ee36
Luiz F do Santos <luiz.santos26@estudante.ifms.edu.br>
2026-09-17 21:31:29 -0400

    Add files via upload

---
+++ consulta/excluir_consulta.php
@@ -0,0 +1,11 @@
+<?php
+
+    $id = $_GET['id'];
+
+    include "funcao.php";
+
+    deleteConsulta($id);
+
+    header('location: consultas.php');
+
+?>
---
+++ consulta/salvar_consulta.php
@@ -0,0 +1,21 @@
+<?php
+
+    $dados = array();
+
+    $dados['data_hora'] = $_POST['data_hora'];
+
+    $dados['diagnostico'] = $_POST['diagnostico'];
+
+    $dados['id_medico'] = $_POST['id_medico'];
+
+    $dados['id_paciente'] = $_POST['id_paciente'];
+
+    $dados['gravidade'] = $_POST['gravidade'];
+
+    include "funcao.php";
+
+    insertConsulta($dados);
+
+    header('location: consultas.php');
+
+?>
