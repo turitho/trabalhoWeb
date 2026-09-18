d79ec7ee600f628daeefd6041be0f97e9115ff34
Luiz F do Santos <luiz.santos26@estudante.ifms.edu.br>
2026-09-18 07:59:04 -0400

    Update formulario_consulta.php

--- consulta/formulario_consulta.php
+++ consulta/formulario_consulta.php
@@ -1,121 +1,49 @@
 <?php
-
-include "gravidade_consulta.php";
-include "funcao.php";
-
-$medicos = getMedicos();
-$pacientes = getPacientes();
-
+  // Previne erro de variável indefinida ao abrir direto em "Nova Consulta"
+  if(!isset($dados)){
+      $dados = array('id' => '', 'data_hora' => '', 'diagnostico' => '', 'id_medico' => '', 'id_paciente' => '', 'gravidade' => '');
+  }
 ?>
-
-<!DOCTYPE html>
-
+<!doctype html>
 <html lang="pt-br">
-
-<head>
-
-<meta charset="UTF-8">
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<title>Nova Consulta</title>
-
-<link rel="stylesheet" href="estilo1.css">
-
-</head>
-
-<body>
-
-<h1>Formulário de Consulta</h1>
-
-<form action="salvar_consulta.php" method="post">
-
-    <div>
-
-        <label>Data e Hora</label>
-
-        <input type="datetime-local" id="data_hora" name="data_hora">
-
-    </div>
-
-    <div>
-
-        <label>Paciente</label>
-
-        <select id="id_paciente" name="id_paciente">
-
-            <?php foreach ($pacientes as $paciente): ?>
-
-                <option value="<?= $paciente['ID_Paciente'] ?>">
-                    <?= $paciente['Nome'] ?>
-                </option>
-
-            <?php endforeach; ?>
-
-        </select>
-
-    </div>
-
-    <div>
-
-        <label>Médico</label>
-
-        <select id="id_medico" name="id_medico">
-
-            <?php foreach ($medicos as $medico): ?>
-
-                <option value="<?= $medico['ID_Medico'] ?>">
-                    <?= $medico['Nome'] ?>
-                </option>
-
-            <?php endforeach; ?>
-
-        </select>
-
-    </div>
-
-    <div>
-
-        <label>Gravidade</label>
-
-        <select id="gravidade" name="gravidade">
-
-            <option value="<?= GravidadeConsulta::BAIXA->value ?>">
-                Baixa
-            </option>
-
-            <option value="<?= GravidadeConsulta::MEDIA->value ?>">
-                Média
-            </option>
-
-            <option value="<?= GravidadeConsulta::ALTA->value ?>">
-                Alta
-            </option>
-
-            <option value="<?= GravidadeConsulta::URGENTE->value ?>">
-                Urgente
-            </option>
-
-        </select>
-
-    </div>
-
-    <div>
-
-        <label>Diagnóstico</label>
-
-        <textarea id="diagnostico" name="diagnostico"></textarea>
-
-    </div>
-
-    <div class="centro">
-
-        <button type="submit">Salvar</button>
-
-    </div>
-
-</form>
-
-</body>
-
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Formulário de Consulta</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+  </head>
+  <body class="container mt-3">
+  <h1>Formulário de Consulta</h1>
+    <form action="salvar_consulta.php" method="post">
+       <div class="mb-3">
+            <label class='form-label'>ID</label>
+            <input class='form-control' readonly type="text" id="id" name="id" value="<?php echo $dados['id']; ?>">
+        </div>
+        <div class="mb-3">
+            <label class='form-label'>Data e Hora (Formato: YYYY-MM-DD HH:MM:SS)</label>
+            <input class='form-control' type="text" id="data_hora" name="data_hora" value="<?php echo $dados['data_hora']; ?>">
+        </div>
+        <div class="mb-3">
+            <label class='form-label'>ID do Médico</label>
+            <input class='form-control' type="text" id="id_medico" name="id_medico" value="<?php echo $dados['id_medico']; ?>">
+        </div>
+        <div class="mb-3">
+            <label class='form-label'>ID do Paciente</label>
+            <input class='form-control' type="text" id="id_paciente" name="id_paciente" value="<?php echo $dados['id_paciente']; ?>">
+        </div>
+        <div class="mb-3">
+            <label class='form-label'>Gravidade</label>
+            <input class='form-control' type="text" id="gravidade" name="gravidade" value="<?php echo $dados['gravidade']; ?>">
+        </div>
+        <div class="mb-3">
+            <label class='form-label'>Diagnóstico</label>
+            <textarea rows='5' class='form-control' id="diagnostico" name="diagnostico"><?php echo $dados['diagnostico']; ?></textarea>
+        </div>
+        <div>
+            <button class="btn btn-primary" type="submit">Salvar</button>
+            <a class="btn btn-secondary" href="consultas.php">Cancelar</a>
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+  </body>
 </html>
